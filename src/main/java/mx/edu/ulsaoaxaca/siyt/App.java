8dd23e65fbd8fb7a23229188959c8b63c316b947
José Luis Flores García <33033155+house117@users.noreply.github.com>
2019-06-14 09:51:34 -0500

    New ArropandoAlMundoBranch

--- src/main/java/mx/edu/ulsaoaxaca/siyt/App.java
+++ src/main/java/mx/edu/ulsaoaxaca/siyt/App.java
@@ -8,12 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	SpringApplication.run(App.class, args);
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
 
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
     }
 }
